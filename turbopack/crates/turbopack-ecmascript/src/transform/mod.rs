042a16ca1943c8431091674258ce36678ce49748
Tim Neutkens <tim@timneutkens.nl>
2025-06-20 18:05:37 +0200

    Turbopack: Don't convert script to module when using preset-env

--- turbopack/crates/turbopack-ecmascript/src/transform/mod.rs
+++ turbopack/crates/turbopack-ecmascript/src/transform/mod.rs
@@ -219,33 +219,16 @@ impl EcmascriptInputTransform {
                     },
                 );
 
-                let module_program = std::mem::replace(program, Program::Module(Module::dummy()));
-
-                let module_program = if let Program::Script(Script {
-                    span,
-                    mut body,
-                    shebang,
-                }) = module_program
-                {
-                    Program::Module(Module {
-                        span,
-                        body: body.drain(..).map(ModuleItem::Stmt).collect(),
-                        shebang,
-                    })
-                } else {
-                    module_program
-                };
-
                 // Explicit type annotation to ensure that we don't duplicate transforms in the
                 // final binary
-                *program = module_program.apply((
+                program.mutate((
                     preset_env::transform_from_env::<&'_ dyn Comments>(
-                        top_level_mark,
+                        unresolved_mark,
                         Some(&comments),
                         config,
                         Assumptions::default(),
                     ),
-                    inject_helpers(unresolved_mark),
+                    inject_helpers(top_level_mark),
                 ));
             }
             EcmascriptInputTransform::TypeScript {
